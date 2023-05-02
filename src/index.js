f60b859fbc25338b7200d5dfd34106f2e2ca1c40
danielgripa <112982758+danielgripa@users.noreply.github.com>
2023-05-02 19:38:43 -0300

    Aula 25-04-23

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './pages/App';
+import reportWebVitals from './reportWebVitals';
+import Teste from './pages/Teste';
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "teste",
+    element: <Teste />
+  }
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <RouterProvider router = {router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/Teste.jsx
@@ -0,0 +1,11 @@
+import Menu from "../componentes/Menu";
+
+const Teste = ( ) => (
+    <div id='nova_pagina'>
+        <Menu/>
+        <p>Meu parágrafo do Componente Teste</p>
+
+    </div>
+);
+
+export default Teste;
