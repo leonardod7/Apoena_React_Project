0b197fb395e68124ae7eefff22b4bf73c6bfa864
Ptosa <126522900+Ptosa@users.noreply.github.com>
2023-05-19 18:56:32 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Style.js'; // Arquivo CSS para estilização do componente
+
+const Cabecalho = () => (
+
+)
+/*
+    <Cabecalho>
+      className="header"
+      style={{
+        position: 'absolute',
+        width: '1280px',
+        height: '64px',
+        left: '0px',
+        top: '0px',
+        backgroundColor: '#5B1F53',
+      }}
+    >
+      <div className="left">
+        <img src="caminho-da-imagem.png" alt="Logo" />
+      </div>
+      <div className="right">
+        <div>Elemento 1</div>
+        <div
+          className="button-icon"
+          style={{
+            position: 'absolute',
+            left: '85.29%',
+            right: '0%',
+            top: '0%',
+            bottom: '84.91%',
+          }}
+        >
+          Elemento 2
+        </div>
+      </div>
+    </div>
+  );
+};
+*/
+export default Header;
---
+++ Style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Cabecalho=styled.div`
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    color: white;
+    max-width: 100px;
+    height: auto;
+    margin-left: 10 px;
+      
+    `;
+
+export default Cabecalho; 
