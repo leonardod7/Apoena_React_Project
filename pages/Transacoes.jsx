6cd2164e4e2269ca698ffdae66543f7392639acb
João Araujo <126522782+araujojv@users.noreply.github.com>
2023-05-19 01:09:25 -0300

    Componente Titulo Criado
    para usar nas paginas, seguir conforme adicionei na page appjsx

---
+++ pages/Transacoes.jsx
@@ -0,0 +1,11 @@
+import Extrato from "../components/Extrato/Extrato";
+import Menu from "../components/Menu/Menu";
+
+const Transacoes = () => (
+    <>
+    <Menu />
+    <Extrato />
+    </>
+);
+
+export default Transacoes;
