2a2242b210d55ba76ea7f8651af6f2093b422b65
Pedro Lustosa <pedroplustosa@gmail.com>
2023-05-22 19:27:00 -0300

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,41 +1,19 @@
-import React from 'react';
-import './Style.js'; // Arquivo CSS para estilização do componente
+import {Container,Vmenu,Vperfil,Vsino,Titulo} from "./Style";
+import ImagemMenu from "../../imagens/Menu.png" ;
+import ImagemSino from "../../imagens/Sino.png" ;
+import ImagemPerfil from "../../imagens/Perfil.png" ;
 
-const Cabecalho = () => (
+const Header =() => (
+    <Container>
+       <Vmenu><img src={ImagemMenu} alt="ImagemMenu"/></Vmenu> 
+       <Titulo>Home</Titulo>
+        <Vsino><img src={ImagemSino} alt="ImagemSino"/></Vsino>
+        <Vperfil><img src={ImagemPerfil} alt="ImagemPerfil"/></Vperfil>
+    </Container>
 
-)
-/*
-    <Cabecalho>
-      className="header"
-      style={{
-        position: 'absolute',
-        width: '1280px',
-        height: '64px',
-        left: '0px',
-        top: '0px',
-        backgroundColor: '#5B1F53',
-      }}
-    >
-      <div className="left">
-        <img src="caminho-da-imagem.png" alt="Logo" />
-      </div>
-      <div className="right">
-        <div>Elemento 1</div>
-        <div
-          className="button-icon"
-          style={{
-            position: 'absolute',
-            left: '85.29%',
-            right: '0%',
-            top: '0%',
-            bottom: '84.91%',
-          }}
-        >
-          Elemento 2
-        </div>
-      </div>
-    </div>
-  );
-};
-*/
+
+
+
+);
+
 export default Header;
