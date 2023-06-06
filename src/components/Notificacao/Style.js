2b8584be8d865b63a4b1fe4a2bbec24812081a1d
Pedro Lustosa <pedroplustosa@gmail.com>
2023-06-06 15:25:30 -0300

    Componente "Notificacao"

---
+++ src/components/Notificacao/Style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Caixa = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 272px;
+    height: 265px;
+    border-radius: 25px;
+    background-color: lightgrey;
+`;
+
+export default Caixa 
