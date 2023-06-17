b432ef8787dc268dcfc8b87af5ea14978efb2eeb
Pedro Lustosa <pedroplustosa@gmail.com>
2023-06-17 19:31:09 -0300

    Update Header 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,48 +2,6 @@ import {Container,Vmenu,Vperfil,Vsino,Titulo, SubContainer} from "./Style";
 import ImagemMenu from "../../imagens/Menu.png";
 import ImagemSino from "../../imagens/Sino.png" ;
 import ImagemPerfil from "../../imagens/Perfil.png" ;
-<<<<<<< Updated upstream
-
-const Header =({toggleMenu, isOpen}) => {
-   
- 
-    return(
-        <Container>
-            <SubContainer>
-                <Vmenu
-                    onClick={toggleMenu}
-                    variant="primary"
-                >
-                    <img src={ImagemMenu}
-                        alt="ImagemMenu"
-                    />
-                </Vmenu> 
-                <Titulo>Home</Titulo>
-            </SubContainer>
-            <SubContainer></SubContainer>
-            <SubContainer></SubContainer>
-            <SubContainer>
-                <Vsino>
-                    <img 
-                        src={ImagemSino}
-                        alt="ImagemSino"/>
-                </Vsino>
-                <Vperfil><img src={ImagemPerfil} alt="ImagemPerfil"/></Vperfil>
-            </SubContainer>
-        </Container>
-    )
-};
-
-const Header =() => (
-    <Container>
-       <Vmenu><img src={ImagemMenu} alt="ImagemMenu"/></Vmenu> 
-       <Titulo>Home</Titulo>
-        <Vsino><img src={ImagemSino} alt="ImagemSino"/></Vsino>
-        <Vperfil><img src={ImagemPerfil} alt="ImagemPerfil"/></Vperfil>
-    </Container>
-=======
->>>>>>> Stashed changes
-
 
 const Header =({toggleMenu, isOpen}) => {
    
