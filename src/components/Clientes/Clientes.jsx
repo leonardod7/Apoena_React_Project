bcd1aeb81d73f86abb900b33bb981cc881006513
ivolavacek <ivo.lavacek@gmail.com>
2023-06-19 14:55:12 -0300

    acerto de navegação da tela meios de pagamentos

--- src/components/Clientes/Clientes.jsx
+++ src/components/Clientes/Clientes.jsx
@@ -3,28 +3,35 @@ import { Container, Cabecalho } from "./Style";
 import clientes from "../../data/clientes.json";
 
 
-const Clientes = () => (
-    <>
-        <Cabecalho>
-            Clientes Existentes
-        </Cabecalho>
+const Clientes = ( props ) => {
+    const {toggleCriarFatura} = props;
 
-        <Container>
-            {
-                clientes.map(
-                    (ele, ind) => (
-                        <ItemCliente
-                            key={ind}
-                            nome={ele.nome_cliente}
-                            email={ele.email}
-                            data_criacao={ele.data_criacao}                           
-                        />
+    return (
+        <>
+            <Cabecalho>
+                Clientes Existentes
+            </Cabecalho>
+
+            <Container>
+                {
+                    clientes.map(
+                        (ele, ind) => (
+                            <ItemCliente
+                                key={ind}
+                                nome={ele.nome_cliente}
+                                email={ele.email}
+                                data_criacao={ele.data_criacao}
+                                cpf={ele.cpf}
+                                endereco={ele.endereco}
+                                toggleCriarFatura={props.toggleCriarFatura}                     
+                            />
+                        )
+                        
                     )
-                    
-                )
-            }
-        </Container>
-    </>
-);
+                }
+            </Container>
+        </>
+    );
+};
 
 export default Clientes;
--- src/components/ItemCliente/ItemCliente.jsx
+++ src/components/ItemCliente/ItemCliente.jsx
@@ -1,13 +1,19 @@
 import { Container, Pessoa, Nome, Email, DataCriacao, Botao } from "./Style";
 
-const ItemCliente = ( props ) => (
-    <>
-        <Container>
-            <Nome>{props.nome}</Nome>
-            <Email>{props.email}</Email>
-            <Botao>Criar fatura</Botao>
-        </Container>
-    </>
-);
+const ItemCliente = ( props ) => {
+    const handleButtonClick = () => {
+        props.toggleCriarFatura( props.nome, props.email, props.cpf, props.endereco );
+    };
+
+    return (
+        <>
+            <Container>
+                <Nome>{props.nome}</Nome>
+                <Email>{props.email}</Email>
+                <Botao onClick={handleButtonClick}>Criar fatura</Botao>
+            </Container>
+        </>
+    );
+};
 
 export default ItemCliente;
