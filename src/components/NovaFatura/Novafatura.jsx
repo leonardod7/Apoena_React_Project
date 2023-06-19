f1186418cc9375e19297e159c601cdd54dc44a70
Pedro Lustosa <pedroplustosa@gmail.com>
2023-06-19 00:49:43 -0300

    NovaFatura Responsivo

--- src/components/NovaFatura/Novafatura.jsx
+++ src/components/NovaFatura/Novafatura.jsx
@@ -2,13 +2,13 @@ import {Container,Container2,AdicionarItem,Salvar,MetodoPagamento,Dropdown} from
 
 const NovaFatura = ( props ) => (
     <Container>
-      <h3 style={{paddingLeft:"25px"}}>Nova Fatura</h3>
-        <h5 style={{paddingLeft:"50px"}}>Enviar Para</h5>
-        <h7 style={{paddingLeft:"50px"}}>Nome</h7>
-        <h5 style={{paddingLeft:"50px"}}>Pessoa Fisica/Juridica</h5>
-        <h7 style={{paddingLeft:"50px"}}>Cpf</h7>
-        <h5 style={{paddingLeft:"50px"}}>Endereço</h5>
-        <h7 style={{paddingLeft:"50px"}}>xxxxx-xxx</h7>
+      <h3 style={{paddingLeft:"25px",marginBottom:"0.5%"}}>Nova Fatura</h3>
+        <h5 style={{paddingLeft:"5%",marginTop:"4px",marginBottom:"4px"}}>Enviar Para</h5>
+        <h7 style={{paddingLeft:"5%",marginTop:"4px",marginBottom:"4px"}}>Nome</h7>
+        <h5 style={{paddingLeft:"4%",marginTop:"4px",marginBottom:"4px"}}>Pessoa Fisica/Juridica</h5>
+        <h7 style={{paddingLeft:"4%",marginTop:"4px",marginBottom:"4px"}}>Cpf</h7>
+        <h5 style={{paddingLeft:"4%",marginTop:"4px",marginBottom:"4px"}}>Endereço</h5>
+        <h7 style={{paddingLeft:"4%",marginTop:"4px",marginBottom:"4px"}}>xxxxx-xxx</h7>
         <Container2></Container2>
         <AdicionarItem >Adicionar Item</AdicionarItem>
         <Salvar>Salvar</Salvar>
